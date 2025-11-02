943d95c88a705377c9b6e2a977037f583a4116a2
Andreigrd2004 <andreigrd2@gmail.com>
2025-11-02 22:27:23 +0200

    lab2

---
+++ lab2/models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,9 @@
+﻿namespace lab2.models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+    }
+}
